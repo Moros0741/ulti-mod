313cd9d019406ecb6dad7258e70bbc801b7d6c8f
Moros0741 <71281300+Moros0741@users.noreply.github.com>
2020-10-27 14:28:52 -0400

    Add files via upload

---
+++ ultibot.py
@@ -0,0 +1,51 @@
+import os
+
+import discord
+from discord.ext import commands
+
+startup_extensions = ["botcommands"]
+
+TOKEN = open('codetoken.txt', 'r').read()
+bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
+
+@bot.event
+async def on_connect():
+    print("Signing into discord")
+    print("Loading Commands extension")
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as: {bot.user.name}')
+    print(f'With ID: {bot.user.id}')
+
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
+
+@bot.command()
+async def reload(ctx, extension_name: str):
+    bot.reload_extension(extension_name)
+    await ctx.send(f"Extension: {extension_name}, has been successfully reloaded!")
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+bot.run(TOKEN)
