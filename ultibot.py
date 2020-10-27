e1b8c160b73f90ded1902b03c360fd278b73d5f5
Moros0741 <71281300+Moros0741@users.noreply.github.com>
2020-10-27 16:12:42 -0400

    Update ultibot.py

--- ultibot.py
+++ ultibot.py
@@ -20,25 +20,25 @@ async def on_ready():
     print(f'With ID: {bot.user.id}')
 
 @bot.command()
-async def load(extension_name : str):
+async def load(ctx, extension_name : str):
     """Loads an extension."""
     try:
         bot.load_extension(extension_name)
     except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        await bot.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
         return
-    await bot.say("{} loaded.".format(extension_name))
+    await ctx.send("{} turned on.".format(extension_name))
 
 @bot.command()
-async def unload(extension_name : str):
+async def unload(ctx, extension_name : str):
     """Unloads an extension."""
     bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
+    await ctx.send("{} turned off.".format(extension_name))
 
 @bot.command()
 async def reload(ctx, extension_name: str):
     bot.reload_extension(extension_name)
-    await ctx.send(f"Extension: {extension_name}, has been successfully reloaded!")
+    await ctx.send(f"Extension: {extension_name}, has been successfully reactivated!")
 
 if __name__ == "__main__":
     for extension in startup_extensions:
